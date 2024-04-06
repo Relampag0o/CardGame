b5ea4cf21f987f659e12d681469dd9171f9c27bb
Relampag0o <josemariar.l17@gmail.com>
2024-04-06 16:59:18 +0200

    First commit. Card class created.

---
+++ CuatrolaGame/src/main/java/org/example/Card.java
@@ -0,0 +1,39 @@
+package org.example;
+
+// THIS IS THE SUPERCLASS FOR EACH CARD!
+public class Card {
+    private String suit; // this is the "palo" of the card
+    private int value; // this is the number of the card
+
+
+    // Constructor
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    // GETTERS AND SETTERS:
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
